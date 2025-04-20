08197eb80cdd80da90d4d18f610c5eb1ab07b0c5
siam420borna <siam420borna@gmail.com>
2025-04-20 11:16:45 +0600

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -2,137 +2,126 @@ import motor.motor_asyncio
 from config import Config
 from .utils import send_log
 
+# Database handler for bot settings
 class Database:
 
-def __init__(self, uri, database_name):
-    self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-    self.jishubotz = self._client[database_name]
-    self.col = self.jishubotz.user
-    self.bannedList = self.jishubotz.bannedList
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.jishubotz = self._client[database_name]
+        self.col = self.jishubotz.user
+        self.bannedList = self.jishubotz.bannedList
 
-def new_user(self, id):
-    return dict(
-        _id=int(id),                                   
-        file_id=None,
-        caption=None,
-        prefix=None,
-        suffix=None,
-        metadata=False,
-        metadata_code="By :- @TechifyBots"
-    )
+    # New user template
+    def new_user(self, id):
+        return {
+            '_id': int(id),
+            'file_id': None,
+            'caption': None,
+            'prefix': None,
+            'suffix': None,
+            'metadata': False,
+            'metadata_code': 'By :- @TechifyBots'
+        }
 
-async def add_user(self, b, m):
-    u = m.from_user
-    if not await self.is_user_exist(u.id):
-        user = self.new_user(u.id)
-        await self.col.insert_one(user)            
-        await send_log(b, u)
+    # User management
+    async def add_user(self, b, m):
+        u = m.from_user
+        if not await self.is_user_exist(u.id):
+            user = self.new_user(u.id)
+            await self.col.insert_one(user)
+            await send_log(b, u)
 
-async def is_user_exist(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return bool(user)
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return bool(user)
 
-async def total_users_count(self):
-    count = await self.col.count_documents({})
-    return count
+    async def total_users_count(self):
+        return await self.col.count_documents({})
 
-async def get_all_users(self):
-    all_users = self.col.find({})
-    return all_users
+    async def get_all_users(self):
+        return self.col.find({})
 
-async def delete_user(self, user_id):
-    await self.col.delete_many({'_id': int(user_id)})
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'_id': int(user_id)})
 
-#======================= Thumbnail ========================#
+    # Thumbnail
+    async def set_thumbnail(self, id, file_id):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
 
-async def set_thumbnail(self, id, file_id):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('file_id', None)
 
-async def get_thumbnail(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('file_id', None)
+    # Caption
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
 
-#======================= Caption ========================#
+    async def get_caption(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('caption', None)
 
-async def set_caption(self, id, caption):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+    # Prefix
+    async def set_prefix(self, id, prefix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})
 
-async def get_caption(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('caption', None)
+    async def get_prefix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('prefix', None)
 
-#======================= Prefix ========================#
+    # Suffix
+    async def set_suffix(self, id, suffix):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})
 
-async def set_prefix(self, id, prefix):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'prefix': prefix}})  
+    async def get_suffix(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('suffix', None)
 
-async def get_prefix(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('prefix', None)
+    # Metadata
+    async def set_metadata(self, id, bool_meta):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
 
-#======================= Suffix ========================#
+    async def get_metadata(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata', None)
 
-async def set_suffix(self, id, suffix):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'suffix': suffix}})  
+    # Metadata code
+    async def set_metadata_code(self, id, metadata_code):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
 
-async def get_suffix(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('suffix', None)
+    async def get_metadata_code(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('metadata_code', None)
 
-#======================= Metadata ========================#
+    # Thumbnail size (custom resize)
+    async def set_thumb_size(self, id, size):
+        await self.col.update_one({'_id': int(id)}, {'$set': {'thumb_size': size}})
 
-async def set_metadata(self, id, bool_meta):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'metadata': bool_meta}})
+    async def get_thumb_size(self, id):
+        user = await self.col.find_one({'_id': int(id)})
+        return user.get('thumb_size', None)
 
-async def get_metadata(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('metadata', None)
-
-#======================= Metadata Code ========================#    
-
-async def set_metadata_code(self, id, metadata_code):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'metadata_code': metadata_code}})
-
-async def get_metadata_code(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('metadata_code', None)
-
-#======================= Thumbnail Size ========================#
-
-async def set_thumb_size(self, id, size):
-    await self.col.update_one({'_id': int(id)}, {'$set': {'thumb_size': size}})
-
-async def get_thumb_size(self, id):
-    user = await self.col.find_one({'_id': int(id)})
-    return user.get('thumb_size', None)
-
-#======================= Ban User ========================#
-
-async def ban_user(self, user_id):
-    user = await self.bannedList.find_one({'banId': int(user_id)})
-    if user:
-        return False
-    else:
+    # Ban control
+    async def ban_user(self, user_id):
+        user = await self.bannedList.find_one({'banId': int(user_id)})
+        if user:
+            return False
         await self.bannedList.insert_one({'banId': int(user_id)})
         return True
 
-async def is_banned(self, user_id):
-    user = await self.bannedList.find_one({'banId': int(user_id)})
-    return True if user else False
+    async def is_banned(self, user_id):
+        user = await self.bannedList.find_one({'banId': int(user_id)})
+        return True if user else False
 
-async def is_unbanned(self, user_id):
-    try: 
-        if await self.bannedList.find_one({'banId': int(user_id)}):
-            await self.bannedList.delete_one({'banId': int(user_id)})
-            return True
-        else:
+    async def is_unbanned(self, user_id):
+        try:
+            if await self.bannedList.find_one({'banId': int(user_id)}):
+                await self.bannedList.delete_one({'banId': int(user_id)})
+                return True
             return False
-    except Exception as e:
-        e = f'Fᴀɪʟᴇᴅ ᴛᴏ ᴜɴʙᴀɴ.Rᴇᴀsᴏɴ : {e}'
-        print(e)
-        return e
+        except Exception as e:
+            print(f"Failed to unban: {e}")
+            return False
 
-Create a Database instance
-
+# Create a Database instance
 jishubotz = Database(Config.DB_URL, Config.DB_NAME)
 

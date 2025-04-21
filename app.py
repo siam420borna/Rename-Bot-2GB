477528f3e1dbeb367da6e12d21bab21faf05f6df
siam420borna <siam420borna@gmail.com>
2025-04-21 11:19:28 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'TechifyBots'
 
-
 if __name__ == "__main__":
-    app.run()
+    # Render থেকে প্রাপ্ত পোর্ট ব্যবহার করুন, যদি না থাকে তাহলে 5000 পোর্টে রান করবে
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  # host="0.0.0.0" দিয়ে সকল অ্যাড্রেস থেকে অ্যাক্সেসযোগ্য করা হবে

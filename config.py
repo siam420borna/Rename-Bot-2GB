2a6244fc3bd73788b942758753f316042004ee09
siam420borna <siam420borna@gmail.com>
2025-04-22 15:27:25 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import os, time, re
+from os import environ
+
+API = environ.get("API", "") # shortlink api
+URL = environ.get("URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+
+
+
 id_pattern = re.compile(r'^.\d+$')
 
 
